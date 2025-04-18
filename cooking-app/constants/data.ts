6d3b8122b950c514ad4d7ee8517afff258b156e5
= <=>
2025-04-18 09:22:05 -0400

    goofagery

---
+++ cooking-app/constants/data.ts
@@ -0,0 +1,47 @@
+export const posts = [
+    {
+        username: "@username",
+        date: "mm/dd/yy",
+        recipeName: "Recipe Name #1",
+        description:
+            "blah blah blah this is some description about recipe name #1",
+        likes: 0,
+        comments: 0,
+    },
+    {
+        username: "@username",
+        date: "mm/dd/yy",
+        recipeName: "Recipe Name #2",
+        description:
+            "blah blah blah this is some description about recipe name #2",
+        likes: 3,
+        comments: 1,
+    },
+    {
+        username: "@username",
+        date: "mm/dd/yy",
+        recipeName: "Recipe Name #3",
+        description:
+            "blah blah blah this is some description about recipe name #3",
+        likes: 3,
+        comments: 1,
+    },
+    {
+        username: "@username",
+        date: "mm/dd/yy",
+        recipeName: "Recipe Name #4",
+        description:
+            "blah blah blah this is some description about recipe name #4",
+        likes: 3,
+        comments: 1,
+    },
+    {
+        username: "@username",
+        date: "mm/dd/yy",
+        recipeName: "Recipe Name #5",
+        description:
+            "blah blah blah this is some description about recipe name #5",
+        likes: 3,
+        comments: 1,
+    },
+];
