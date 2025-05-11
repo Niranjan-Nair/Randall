ce3399a61d8ecc9361a1206cd8f9530572ad163b
= <=>
2025-05-10 21:09:16 -0400

    near miss

--- cooking-app/scripts/firebase.ts
+++ cooking-app/scripts/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDGDMRm_dhNtC-PfpautF13-audDtaoadw",
-    authDomain: "cooking-app-99649.firebaseapp.com",
-    projectId: "cooking-app-99649",
-    storageBucket: "cooking-app-99649.firebasestorage.app",
-    messagingSenderId: "736932998014",
-    appId: "1:736932998014:web:089e8c1b2ec391baca212f",
-    measurementId: "G-7XVDD0YY72"
+    apiKey: process.env.EXPO_PUBLIC_APIKEY,
+    authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
+    projectId: process.env.EXPO_PUBLIC_PROJECTID,
+    storageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
+    appId: process.env.EXPO_PUBLIC_APPID,
+    measurementId: process.env.EXPO_PUBLIC_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
